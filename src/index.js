ef832f279673e1c8a124b82745f0259aee7b446d
TheSirsh <sirsh@bk.ru>
2022-06-28 16:48:45 +0300

    feat: the test is done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let rvrs = "";
+    x =String(n);
+
+    for (let i = (x.length - 1); i >= 0; i--)  
+        {
+        rvrs = rvrs + x[i];
+        }
+    n = parseInt(rvrs);
+	return n
 }
